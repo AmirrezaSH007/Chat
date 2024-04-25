abfa27a74ec5b690613f93bcff0f1230c1d1dfcc
AmirrezaSH007 <165849887+AmirrezaSH007@users.noreply.github.com>
2024-04-24 17:58:55 -0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,39 +1,63 @@
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URI;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    int num = 0;
+interface URLHandler {
+    String handleRequest(URI url);
+}
+
+class ServerHttpHandler implements HttpHandler {
+    URLHandler handler;
+    
+    ServerHttpHandler(URLHandler handler) {
+        this.handler = handler;
+    }
+    
+    public void handle(final HttpExchange exchange) throws IOException {
+        try {
+            String response = handler.handleRequest(exchange.getRequestURI());
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.getResponseBody().close();
+        } catch (Exception e) {
+            String response = "Internal Server Error: " + e.getMessage();
+            exchange.sendResponseHeaders(500, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.getResponseBody().close();
+        }
+    }
+}
+
+class ChatHandler implements URLHandler {
+    private StringBuilder chatHistory = new StringBuilder();
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+        if (url.getPath().equals("/add-message")) {
+            String query = url.getQuery();
+            String[] params = query.split("&");
+            String message = params[0].split("=")[1].replace("+", " ");
+            String user = params[1].split("=")[1].replace("+", " ");
+            chatHistory.append(user).append(": ").append(message).append("\n");
+            return chatHistory.toString();
         } else {
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
             return "404 Not Found!";
         }
     }
 }
 
-class ChatServer {
+public class ChatServer {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new ServerHttpHandler(new ChatHandler()));
+        server.start();
+        System.out.println("Server started on port " + port + ". Visit http://localhost:" + port + "/add-message?s=YourMessage&user=YourName to add messages.");
     }
 }
