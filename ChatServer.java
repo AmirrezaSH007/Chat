9bf107e41bfd902e719f081e97a5d0632e76675a
AmirrezaSH007 <165849887+AmirrezaSH007@users.noreply.github.com>
2024-04-24 18:14:56 -0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -34,12 +34,15 @@ class Handler implements URLHandler {
         }
     }
 }
-public class ChatServer {
+class ChatServer {
     public static void main(String[] args) throws IOException {
-        int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", new ServerHttpHandler(new ChatHandler()));
-        server.start();
-        System.out.println("Server started on port " + port);
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
     }
 }
